c6c9b93266de8d501f08b9307f8667c0b4d1bb7a
afinesami <samibenomar84@gmail.com>
2019-12-17 21:53:04 +0000

    print reversed list integer

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return(my_list)
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
+    for i in reversed(my_list):
+        print('{:d}'.format(i))
