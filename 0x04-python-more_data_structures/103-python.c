b67c817b6eb22d8ea0b52b7a85b9f350f58584c7
afinesami <samibenomar84@gmail.com>
2019-12-19 21:00:35 +0000

    Data Structures

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,26 @@
+#include "Python.h"
+/**
+ * print_python_list_info - Prints information about python objects
+ * @p: PyObject pointer to print info about
+ * Compile with:
+ * gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared -Wl,-soname,libPython.so -o libPython.so -fPIC -I/usr/include/python3.4 103-python.c
+ */
+void print_python_list_info(PyObject *p)
+{
+  Py_ssize_t i, py_list_size;
+  PyObject *item;
+  const char *item_type;
+  PyListObject *list_object_cast;
+
+  list_object_cast = (PyListObject *)p;
+  py_list_size = PyList_Size(p);
+
+  printf("[*] Size of the Python List = %d\n", (int) py_list_size);
+  printf("[*] Allocated = %d\n", (int)list_object_cast->allocated);
+  for (i = 0; i < py_list_size; i++)
+    {
+      item = PyList_GetItem(p, i);
+      item_type = (PyObject*(item))->ob_size->tp_name;
+      printf("Element %d: %s\n", (int) i, item_type);
+    }
+}
