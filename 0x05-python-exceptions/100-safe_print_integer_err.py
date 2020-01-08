91c9212018af03d66fef98dfb9257f903c567a8e
afinesami <samibenomar84@gmail.com>
2020-01-08 19:34:13 +0000

    safe print integer err

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import sys
+def safe_print_integer_err(value):
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    except TypeError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
