7aa8078100f6606f7ce58bcbd1f36540e6948c62
afinesami <samibenomar84@gmail.com>
2020-01-13 22:05:43 +0000

    add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+add_integer module
+'''
+
+
+def add_integer(a, b):
+
+    """
+    Return the sum of a and b.
+    a -- first integer
+    b -- second integer
+    """
+
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+
+    if not isinstance(a, int) or isinstance(a, bool):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) or isinstance(b, bool):
+        raise TypeError("b must be an integer")
+
+    return (a + b)
