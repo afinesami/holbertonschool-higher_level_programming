bec63b96ae1a9887cc690b870ac6e9eb55a72f4d
afinesami <samibenomar84@gmail.com>
2020-01-19 19:37:50 +0000

    say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 '''
-module: 3-say_my_name
+module: say_my_name
 '''
 
 
 def say_my_name(first_name, last_name=''):
+    ''' print first and last name, make the big bucks
+    Keyword arguments:
+    first_name -- string
+    last_name -- string
+    '''
+
+    #  ERROR MESSAGE DICT  #
+    err_msg = {}
+    err_msg["FirstNotStr"] = "first_name must be a string"
+    err_msg["LastNotStr"] = "last_name must be a string"
+
+    #  TESTS  #
+    if type(first_name) != str:
+        raise TypeError(err_msg["FirstNotStr"])
+    if type(last_name) != str:
+        raise TypeError(err_msg["LastNotStr"])
+
+    #  OUTPUT  #
+    print("My name is {}".format(first_name), end='')
+    if len(last_name) == 0:
+        print()
+    else:
+        print(" {}".format(last_name))
