9e6fa6f2e456e885ded5b59777822ab6923dfc72
afinesami <samibenomar84@gmail.com>
2020-01-19 20:59:51 +0000

    matrix mul

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "matrix_mul" module.
+Module functions: matrix_mul(m_a, m_b).
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    print_square: prints a square with the character #.
+    size (int): multiplies 2 matrices
+    """
+    return matrix
