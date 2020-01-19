965f9b98b89cb5f886585fee5344f2f1e5fa2b77
afinesami <samibenomar84@gmail.com>
2020-01-19 20:41:36 +0000

    max integer test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,79 +8,75 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Presence of docstrings"""
 
-    def test_presence_of_module_docstring(self):
-        test = __import__('6-max_integer').__doc__
-        self.assertTrue(len(test) > 1)
+    def test_module_docstring(self):
+        """Check: module docsting"""
+        module = __import__('6-max_integer').__doc__
+        self.assertTrue(len(module) > 1)
 
-    def test_presence_of_func_docstring(self):
-        test = max_integer.__doc__
-        self.assertTrue(len(test) > 1)
+    def test_function_docstring(self):
+        """Check: funstion docstring"""
+        f = max_integer.__doc__
+        self.assertTrue(len(f) > 1)
 
-    """Working results"""
+    def test_no_args(self):
+        """Check: no arguments passed to func"""
+        self.assertIsNone(max_integer())
+
     def test_empty_list(self):
+        """Check: empty list []"""
         test = []
         self.assertIsNone(max_integer(test))
 
-    def test_no_arguments(self):
-        self.assertIsNone(max_integer())
+    def test_max_at_end(self):
+        """Check: all positive max at end"""
+        test = [12, 1, 18, 6, 14, 40]
+        self.assertEqual(max_integer(test), 40)
 
-    def test_one_element(self):
-        test = [1]
-        self.assertEqual(max_integer(test), 1)
+    def test_two_of_max(self):
+        """Check: max at end"""
+        test = [12, 1, 40, 6, 14, 40]
+        self.assertEqual(max_integer(test), 40)
 
-    def test_some_elements(self):
-        test = [3425, 457458, 1, 3249056, -2346, 0]
-        self.assertEqual(max_integer(test), 3249056)
-
-    def test_floats(self):
-        test = [23.2354, 635.34, 234658, 99, -102346.052]
-        self.assertEqual(max_integer(test), 234658)
-
-    def test_big_numbers(self):
-        big = 1000000000000000000000000000000000000000000000000
-        smal = -1000000000000000000000000000000000000000000000000
-        test = [big, smal]
-        self.assertEqual(max_integer(test), big)
-
-    def test_pos_infinity(self):
-        test = [1, 2, float('inf'), 9]
-        self.assertEqual(max_integer(test), float('inf'))
-
-    def test_neg_infinity(self):
-        test = [1, 2, -float('inf'), 9]
-        self.assertEqual(max_integer(test), 9)
-
-    def test_custom_int_subclass(self):
-        class ChildInt(int):
-            pass
-        big = ChildInt(456563723456365843)
-        number = [1, 2, 3, big_child]
-        self.assertEqual(max_integer(number), big)
-
-    def test_boolean(self):
-        '''test boolean
-        '''
-        self.assertEqual(max_integer([True, False]), True)
-
-    """Catching exceptions"""
-    def test_passing_none(self):
+    def test_none(self):
+        """Check: passing none as argument"""
         with self.assertRaises(TypeError):
             max_integer(None)
 
-    def test_passing_string(self):
-        test = [2345, 2, "L0Lz", 9]
+    def test_non_int_arg(self):
+        """Check: non integer type"""
+        test = [1, "string", 3, 4, 5]
         with self.assertRaises(TypeError):
             max_integer(test)
 
-    def test_passing_list(self):
-        test = [2345, 2, [], 9]
-        with self.assertRaises(TypeError):
-            max_integer(test)
+    def test_one_element(self):
+        """Check: only one number"""
+        test_ele = [10]
+        self.assertEqual(max_integer(test_ele), 10)
 
-    def test_passing_list_one_int(self):
-        test = [666, [1337], 99]
-        with self.assertRaises(TypeError):
-            max_integer(test)
+    def test_max_at_beginning(self):
+        """Check: positive max at beginning"""
+        test_pos = [130, 129, 23, 6, 0, 10]
+        self.assertEqual(max_integer(test_pos), 130)
+
+    def test_max_at_middle(self):
+        """Check: max in middle"""
+        test_max = [1, 10, 23, 30, 14, 12]
+        self.assertEqual(max_integer(test_max), 30)
+
+    def test_one_negative(self):
+        """Check: negative number"""
+        test = [10, -10, 1, 6, 4, 2]
+        self.assertEqual(max_integer(test), 10)
+
+    def test_all_10(self):
+        """Check: all otems """
+        test = [10, 10, 10, 10, 10, 10]
+        self.assertEqual(max_integer(test), 10)
+
+    def test_all_negative(self):
+        """Check: all negative numbers"""
+        test_neg = [-1, -2, -3, -4, -5]
+        self.assertEqual(max_integer(test_neg), -1)
 
 
 if __name__ == "__main__":
