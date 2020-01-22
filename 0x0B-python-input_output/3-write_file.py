38902b1775ef58d08a013ddda495c4d777399232
afinesami <samibenomar84@gmail.com>
2020-01-22 22:39:51 +0000

    read and write

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that writes a string to a text file (UTF8) and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w') as f:
+        return f.write(text)
