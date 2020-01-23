09994dbe29e02424f443fb5c238fe0f6b98fc4f9
afinesami <samibenomar84@gmail.com>
2020-01-23 08:29:13 +0000

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,10 @@
 #!/bin/python3
-''' module: 0-lookup
+''' function that returns the list
 '''
 
 
 def lookup(obj):
     ''' function: lookup()
-    obj: an object
     Returns a list object
     '''
     return dir(obj)
