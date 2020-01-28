ea8ee177e650a52b8f78bda42d3540812775a204
afinesami <samibenomar84@gmail.com>
2020-01-28 23:08:20 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,59 +1,64 @@
-#.rectangle import Rectangle
+#!/usr/bin/python3
+"""Define Rectangle Class
+"""
+
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square Class Reclangle
+    """Module Representation of Square
 """
-    def __init__(self, size, x=0, y=0, id=None):
-        """method : init magic 
-"""
-        super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """method : str magic 
-"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".\
-            format(self.id, self.x, self.y, self.width)
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization a Square
+        """
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """method : size getter
-"""
+        """module Square size getter
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """size setter method"""
+        """module Square size setter
+        """
         self.width = value
         self.height = value
 
+    def __str__(self):
+        """module string represation of square
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
+
     def update(self, *args, **kwargs):
-        """method : update
-"""
-        if len(args) > 0:
-            self.id = args[0]
-            if len(args) > 1:
-                self.size = args[1]
-            if len(args) > 2:
-                self.x = args[2]
-            if len(args) > 3:
-                self.y = args[3]
+        """module update square
+        """
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
         else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """method : to_dictionary
-"""
-        n_dictionar = {}
-        n_dictionar["id"] = self.id
-        n_dictionar["size"] = self.size
-        n_dictionar["x"] = self.x
-        n_dictionar["y"] = self.y
-        return n_dictionar
+        """retrun dictonary
+        """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
