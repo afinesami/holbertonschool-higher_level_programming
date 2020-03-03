06810aba06543ca693252fa7d227ae475a6b21a1
Lafine Sami <lafine.sami@gmail.com>
2020-03-03 22:24:42 +0000

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL server user user_0d_1
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
