b736f5b8f0150bf32ca0287ffa296fa161ef7a44
afinesami <samibenomar84@gmail.com>
2020-04-16 23:20:44 +0100

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Python script that fetches https://intranet.hbtn.io/status
+# Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
