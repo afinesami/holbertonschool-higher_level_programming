d094c5389f17525018dd12f678f7fa3e341d1371
afinesami <samibenomar84@gmail.com>
2020-05-11 16:59:45 +0000

    rectagle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i, j, string;
+    for (i = 0; i < this.height; i++) {
+      string = '';
+      for (j = 0; j < this.width; j++) {
+        string += 'X';
+      }
+      console.log(string);
+    }
+  }
+
+  rotate () {
+    let temp;
+    temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
+}
+
+module.exports = Rectangle;
