65bfb7c447237d6535a64c18088e6d3f1b27aa9a
afinesami <samibenomar84@gmail.com>
2020-05-12 19:15:58 +0000

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 // prints the title of a Star Wars movie where the episode num matches given int
 
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
