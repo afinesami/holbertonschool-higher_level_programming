680204f49dcfb8c81a7788b91870b4989c09eecd
afinesami <samibenomar84@gmail.com>
2020-05-12 19:11:32 +0000

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-// script that prints the title of a Star Wars movie
+// prints the title of a Star Wars movie where the episode num matches given int
 
-const args = process.argv;
-let reqURL = 'http://swapi.co/api/films/' + args[2] + '/';
-let request = require('request');
-request(reqURL, function (error, response, body) {
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   if (error) {
-    console.log('error:', error);
+    console.error(error);
   } else {
-    let jso = JSON.parse(body);
-    console.log(jso['title']);
+    console.log(JSON.parse(body).title);
   }
 });
