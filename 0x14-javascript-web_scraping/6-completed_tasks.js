ad16c68401bd8975fc48197fda069ba4c4d85978
afinesami <samibenomar84@gmail.com>
2020-05-12 19:22:18 +0000

    complete task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const args = process.argv;
+let reqURL = args[2];
+let request = require('request');
+request(reqURL, function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    let todos = JSON.parse(body);
+    let dash = {};
+    for (let i = 0; i < todos.length; i++) {
+      let status = (todos[i]['completed']);
+      let key = todos[i]['userId'].toString();
+      if (status) {
+        if (dash[key]) {
+          dash[key]++;
+        } else {
+          dash[key] = 1;
+        }
+      }
+    }
+    console.log(dash);
+  }
+});
