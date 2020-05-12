48faa11f3519d1bbc8bf2c88eadac91198680a5c
afinesami <samibenomar84@gmail.com>
2020-05-12 19:08:38 +0000

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,6 @@ const args = process.argv;
 let reqURL = 'http://swapi.co/api/films/' + args[2] + '/';
 let request = require('request');
 request(reqURL, function (error, response, body) {
-// Print the error if one occurred
   if (error) {
     console.log('error:', error);
   } else {
