cbe437f7ea13970585e477a20028a282bd368015
afinesami <samibenomar84@gmail.com>
2020-06-22 12:26:35 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     r = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text
+    print("\t- content: {}".format(r.text))
