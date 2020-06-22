35af62e50ede9fd3948cf92a6ad1e5058ffeb5db
afinesami <samibenomar84@gmail.com>
2020-06-22 11:34:33 +0000

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
-// Script that gets the contents of a webpage and stores it in a file.
-
-const request = require('request');
 const fs = require('fs');
-const file = process.argv[3];
-const url = process.argv[2];
-const write_ = request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(JSON.parse(body));
-  }
-});
-
-fs.writeFile(file, write_, 'utf8', function (error) {
-  if (error) {
-    console.log(error);
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
